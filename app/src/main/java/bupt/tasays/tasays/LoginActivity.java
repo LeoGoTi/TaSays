a41cfaa6be76c885329cfdd8851615c28499b0bd
LeoGoTi <leomas2795@yahoo.com>
2017-11-30 17:12:34 +0800

    version0.1.3

--- app/src/main/java/bupt/tasays/tasays/LoginActivity.java
+++ app/src/main/java/bupt/tasays/tasays/LoginActivity.java
@@ -1,14 +1,18 @@
 package bupt.tasays.tasays;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static java.lang.Thread.sleep;
 
 public class LoginActivity extends AppCompatActivity {
     private int backPressed=0;
@@ -20,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         TextView register,forget;
-        Button login;
+        final Button login;
         register = findViewById(R.id.login_register);
         forget   = findViewById(R.id.login_forget);
         login    = findViewById(R.id.login_button);
@@ -51,9 +55,40 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 //Logic of login here;
-                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                final Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+
+                final ProgressDialog progressDialog=new ProgressDialog(LoginActivity.this);
+                progressDialog.setTitle("登陆中");
+                progressDialog.setMessage("请稍等……");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                /*下面开启新线程*/
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        int i = 0;
+
+                        while (i<100) {
+                            try
+                            {
+                                Log.d("LoginActivity", "run: ");
+                                i++;
+                                sleep(10);
+                            }
+                            catch (Exception e) {
+                                // TODO: handle exception
+                            }
+                        }
+                        // 在进度条走完时删除Dialog,进入下一个活动
+                        progressDialog.dismiss();
+                        startActivity(intent);
+                        finish();
+
+                    }
+                }).start();
+
             }
         });
     }
--- app/src/main/java/bupt/tasays/tasays/MainActivity.java
+++ app/src/main/java/bupt/tasays/tasays/MainActivity.java
@@ -4,46 +4,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-   ImageView i1,i2,i3;
+   LinearLayout i1,i2,i3;
+   ImageView p1,p2,p3;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        p1=findViewById(R.id.main_music_p);
+        p2=findViewById(R.id.main_home_p);
+        p3=findViewById(R.id.main_personal_p);
 
-        i1=findViewById(R.id.main_cd);
+        i1=findViewById(R.id.main_music);
         i1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                i1.setImageResource(R.drawable.cd);
-                i2.setImageResource(R.drawable.laba_1);
-                i3.setImageResource(R.drawable.fangzi_1);
+                p1.setImageResource(R.drawable.yinyue);
+                p2.setImageResource(R.drawable.zhuye_1);
+                p3.setImageResource(R.drawable.geren_1);
             }
         });
-        i2=findViewById(R.id.main_laba);
+        i2=findViewById(R.id.main_home);
         i2.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                i1.setImageResource(R.drawable.cd_1);
-                i2.setImageResource(R.drawable.laba);
-                i3.setImageResource(R.drawable.fangzi_1);
+                p1.setImageResource(R.drawable.yinyue_1);
+                p2.setImageResource(R.drawable.zhuye);
+                p3.setImageResource(R.drawable.geren_1);
             }
         });
-        i3=findViewById(R.id.main_house);
+        i3=findViewById(R.id.main_personal);
         i3.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                i1.setImageResource(R.drawable.cd_1);
-                i2.setImageResource(R.drawable.laba_1);
-                i3.setImageResource(R.drawable.fangzi);
+                p1.setImageResource(R.drawable.yinyue_1);
+                p2.setImageResource(R.drawable.zhuye_1);
+                p3.setImageResource(R.drawable.geren);
             }
         });
     }
